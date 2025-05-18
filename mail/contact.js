f4d00c667959f5f7b5691f23c366be5a137af6d6
Pecoden <90499770+0WEBID0@users.noreply.github.com>
2025-05-18 17:43:46 +0300

    social

--- mail/contact.js
+++ mail/contact.js
@@ -1,35 +1,44 @@
 $(function () {
 
-    $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
+    $("#contactForm input, #contactForm textarea, #contactForm select").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function ($form, event, errors) {
         },
         submitSuccess: function ($form, event) {
             event.preventDefault();
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var subject = $("input#subject").val();
-            var message = $("textarea#message").val();
+            var formData = {
+                name: $("input[name='name']").val(),
+                email: $("input[name='email']").val(),
+                lastname: $("input[name='lastname']").val(),
+                from_address: $("input[name='from_address']").val(),
+                postcode: $("input[name='postcode']").val(),
+                from_floor: $("select[name='from_floor']").val(),
+                to_address: $("input[name='to_address']").val(),
+                to_floor: $("select[name='to_floor']").val(),
+                time: $("select[name='time']").val(),
+                date: $("input[name='date']").val(),
+                lift_type: $("select[name='lift_type']").val(),
+                move_floor: $("select[name='move_floor']").val(),
+                move_hour: $("select[name='move_hour']").val(),
+                move_price: $("select[name='move_price']").val(),
+                phone: $("input[name='phone']").val(),
+                message: $("textarea[name='message']").val()
+            };
 
             $this = $("#sendMessageButton");
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contact.php",
+                url: "mail/contact.php",
                 type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    subject: subject,
-                    message: message
-                },
+                data: formData,
                 cache: false,
-                success: function () {
+                success: function (response) {
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
                     $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
+                            .append("<strong>Uw bericht is succesvol verzonden. </strong>");
                     $('#success > .alert-success')
                             .append('</div>');
                     $('#contactForm').trigger("reset");
@@ -38,7 +47,7 @@ $(function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Sorry, het lijkt erop dat onze mailserver niet reageert. Probeer het later opnieuw!"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
--- mail/contact.php
+++ mail/contact.php
@@ -1,20 +1,56 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+if(
+    empty($_POST['name']) || empty($_POST['email']) ||
+    !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)
+) {
   http_response_code(500);
   exit();
 }
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+$lastname = isset($_POST['lastname']) ? strip_tags(htmlspecialchars($_POST['lastname'])) : '';
+$from_address = isset($_POST['from_address']) ? strip_tags(htmlspecialchars($_POST['from_address'])) : '';
+$postcode = isset($_POST['postcode']) ? strip_tags(htmlspecialchars($_POST['postcode'])) : '';
+$from_floor = isset($_POST['from_floor']) ? strip_tags(htmlspecialchars($_POST['from_floor'])) : '';
+$to_address = isset($_POST['to_address']) ? strip_tags(htmlspecialchars($_POST['to_address'])) : '';
+$to_floor = isset($_POST['to_floor']) ? strip_tags(htmlspecialchars($_POST['to_floor'])) : '';
+$time = isset($_POST['time']) ? strip_tags(htmlspecialchars($_POST['time'])) : '';
+$date = isset($_POST['date']) ? strip_tags(htmlspecialchars($_POST['date'])) : '';
+$lift_type = isset($_POST['lift_type']) ? strip_tags(htmlspecialchars($_POST['lift_type'])) : '';
+$move_floor = isset($_POST['move_floor']) ? strip_tags(htmlspecialchars($_POST['move_floor'])) : '';
+$move_hour = isset($_POST['move_hour']) ? strip_tags(htmlspecialchars($_POST['move_hour'])) : '';
+$move_price = isset($_POST['move_price']) ? strip_tags(htmlspecialchars($_POST['move_price'])) : '';
+$phone = isset($_POST['phone']) ? strip_tags(htmlspecialchars($_POST['phone'])) : '';
+$message = isset($_POST['message']) ? strip_tags(htmlspecialchars($_POST['message'])) : '';
 
-$to = "Hayderalkubasy89@gmail.com"; // Change this email to your //
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
+$to = "info@liederserivce.be"; // Change this email to your //
+$subject = "Nieuw contactformulier van $name $lastname";
+$body = "U heeft een nieuw bericht ontvangen van het contactformulier op uw website.\n\n";
+$body .= "Gegevens klant:\n";
+$body .= "Naam: $name\n";
+$body .= "Achternaam: $lastname\n";
+$body .= "E-mail: $email\n";
+$body .= "Telefoonnummer: $phone\n";
+$body .= "Adres van vertrek: $from_address\n";
+$body .= "Postcode: $postcode\n";
+$body .= "Van Verdieping: $from_floor\n";
+$body .= "Verhuizen Naar: $to_address\n";
+$body .= "Naar Verdieping: $to_floor\n";
+$body .= "Datum: $date\n";
+$body .= "Tijd: $time\n";
+$body .= "Type van Lift: $lift_type\n";
+$body .= "Verdieping verhuis: $move_floor\n";
+$body .= "Uur verhuis: $move_hour\n";
+$body .= "Prijs verhuis: $move_price\n";
+$body .= "Bericht:\n$message\n";
 
-if(!mail($to, $subject, $body, $header))
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+
+if(!mail($to, $subject, $body, $headers)) {
   http_response_code(500);
+  exit();
+}
+http_response_code(200);
 ?>
